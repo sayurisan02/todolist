d66f9886b9684cf3bcae7d9d547ed14fb5ca3e90
7d22 <s.s261.y@gmail.com>
2024-12-17 14:12:12 +0900

    最初

---
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -0,0 +1,65 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/hello")
+    String hello(Model model){
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
+    }
+
+    record TaskItem(String id, String task, String deadline, boolean done){}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline){
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        dao.add(item);
+
+        return "redirect:/list";
+    }
+
+    private final TaskListDao dao;
+
+    @Autowired
+    HomeController(TaskListDao dao){
+        this.dao = dao;
+    }
+
+    @GetMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
+        dao.delete(id);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/update")
+    String updateItem(@RequestParam("id") String id,
+                      @RequestParam("task") String task,
+                      @RequestParam("deadline") String deadline,
+                      @RequestParam("done") boolean done) {
+        TaskItem taskItem = new TaskItem(id, task, deadline, done);
+        dao.update(taskItem);
+        return "redirect:/list";
+    }
+    @GetMapping("/list")
+    String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
+        model.addAttribute("taskList", taskItems);
+        return "home";
+    }
+}
